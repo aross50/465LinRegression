04091b56f9e3696c043185008716c719b9d20739
aross50 <149820800+aross50@users.noreply.github.com>
2024-04-13 14:25:48 -0500

    Initial Commit
    Initial Commit
    --Already tried a few implementations, already optimized from 100+ cycles to 70 to 44 to 40.
    --Currently have rather direct translation of algorithm
    --Some optimizations present(loop unrolling, adder pyramids etc)
    --Currently only using fixed size training data, which matches paper
    
    Ideas for future things
    --Want to try for streaming inputs for larger datasets
    --Paper mentioned approximate calculation of inverse of Determinant, which is currently the largest slow down (23 of 40 cycles)

---
+++ calcXtX.cpp
@@ -0,0 +1,98 @@
+//Attempt at a direct implementation; watch out though this is currently using ints.
+// TODO: Fixed point made it 44 cycles; optimizations probable
+//       --Saved another four changing from sat to wrap. Will be fine as long as we don't make ridiculously large numbers
+//		 --Can try using different sizes for inputs and outputs for saving some space/Make room for division alternative
+// TODO: Set up Git repositories for all this
+// TODO: Find a way to Approximate division for S (it is highest latency by far). Time for good ol friend approx. comp?
+
+#include <ap_fixed.h>
+
+//Play around with different rounding/overflow values, see if that speeds some things up (such as the division?)
+typedef ap_fixed<32, 16, AP_TRN, AP_WRAP> fix;
+
+void calcXTX(fix x[8], fix y[8], fix& theta0, fix& theta1){
+#pragma HLS INTERFACE ap_ctrl_none port=return
+#pragma HLS INTERFACE s_axilite port=x
+#pragma HLS INTERFACE s_axilite port=y
+#pragma HLS INTERFACE s_axilite port=theta0
+#pragma HLS INTERFACE s_axilite port=theta1
+#pragma HLS ARRAY_PARTITION variable=x
+#pragma HLS ARRAY_PARTITION variable=y
+
+	fix n = 0;
+	fix a[8];
+	fix b[8];
+
+	for(int i = 0; i<8; i++){
+		#pragma HLS UNROLL
+		n +=  1;
+		a[i] =  x[i];
+		b[i] = (x[i] * x[i]);
+	}
+
+	//A
+	a[6] += a[7];
+	a[4] += a[5];
+	a[2] += a[3];
+	a[0] += a[1];
+
+	a[4] += a[6];
+	a[0] += a[2];
+
+	a[0] += a[4];
+
+
+	//B
+	b[6] += b[7];
+	b[4] += b[5];
+	b[2] += b[3];
+	b[0] += b[1];
+
+	b[4] += b[6];
+	b[0] += b[2];
+
+	b[0] += b[4];
+
+
+	// should check if putting in one line is faster for whatever reason
+	fix nb = n    * b[0];
+	fix a2 = a[0] * a[0];
+	fix s  = 1/(nb-a2);
+
+
+	fix c =  b[0] * s;
+	fix d = -a[0] * s;
+	fix e =  n    * s;
+
+	fix th0[8];
+	fix th1[8];
+	for(int i = 0; i < 8; i++){
+		#pragma HLS UNROLL
+		th0[i] += (c + (d*x[i]))*y[i];
+		th1[i] += (d + (e*x[i]))*y[i];
+	}
+	//Theta0
+	th0[6] += th0[7];
+	th0[4] += th0[5];
+	th0[2] += th0[3];
+	th0[0] += th0[1];
+
+	th0[4] += th0[6];
+	th0[0] += th0[2];
+
+	theta0 = th0[0] + th0[4];
+
+
+	//Theta1
+	th1[6] += th1[7];
+	th1[4] += th1[5];
+	th1[2] += th1[3];
+	th1[0] += th1[1];
+
+	th1[4] += th1[6];
+	th1[0] += th1[2];
+
+	theta1 = th1[0] + th1[4];
+
+
+}
